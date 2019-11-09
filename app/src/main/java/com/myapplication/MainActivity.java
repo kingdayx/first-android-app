6c441419bc5c251a31b027c2ba3ce0ffb4b20eb3
kingdayx <54965488+kingdayx@users.noreply.github.com>
2019-11-09 18:43:20 -0500

    fixed both java files

--- app/src/main/java/com/myapplication/MainActivity.java
+++ app/src/main/java/com/myapplication/MainActivity.java
@@ -10,10 +10,10 @@ import android.widget.TextView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FunFactActivity funFactActivity = new FunFactActivity();
     private TextView mfactTextView;
     private Button showFactButton;
-    String[] facts;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,23 +27,7 @@ public class MainActivity extends AppCompatActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String[] facts = {
-                        "Ants stretch when they wake up in the morning.",
-                        "Ostriches can run faster than horses.",
-                        "Olympic gold medals are actually made mostly of silver.",
-                        "You are born with 300 bones; by the time you are an adult you will have 206.",
-                        "It takes about 8 minutes for light from the Sun to reach Earth.",
-                        "Some bamboo plants can grow almost a meter in just one day.",
-                        "The state of Florida is bigger than England.",
-                        "Some penguins can leap 2-3 meters out of the water.",
-                        "On average, it takes 66 days to form a new habit.",
-                        "Mammoths still walked the earth when the Great Pyramid was being built." };
-
-                Random randomGenerator;
-
-                randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(facts.length);
-                String fact = facts[randomNumber];
+               String fact = funFactActivity.getFacts();
 
                 mfactTextView.setText(fact);
             }
