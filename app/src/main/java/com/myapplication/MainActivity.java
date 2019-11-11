3a6c685fccbd7ff83bfa569e59dea1e62d4dbb7f
kingdayx <54965488+kingdayx@users.noreply.github.com>
2019-11-11 17:13:47 -0500

    added launcher image and fixed java
    fix java so that on button click the background color changes and added launcher image

--- app/src/main/java/com/myapplication/MainActivity.java
+++ app/src/main/java/com/myapplication/MainActivity.java
@@ -2,18 +2,21 @@ package com.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private FunFactActivity funFactActivity = new FunFactActivity();
+    private ColorWheel colorWheel = new ColorWheel();
     private TextView mfactTextView;
     private Button showFactButton;
-
+    private RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +25,16 @@ public class MainActivity extends AppCompatActivity {
 
         mfactTextView = (TextView) findViewById(R.id.factTextView);
         showFactButton = (Button) findViewById(R.id.showFactButton);
-
+        relativeLayout= (RelativeLayout)  findViewById((R.id.relativeLayout));
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                String fact = funFactActivity.getFacts();
-
+                int color = colorWheel.getColor();
                 mfactTextView.setText(fact);
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
             }
         };
         showFactButton.setOnClickListener(listener);
